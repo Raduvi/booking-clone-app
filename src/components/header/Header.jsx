ff5b5eb0829101c314062885b42424563083dda3
Raduvi <patruica.radu@gmail.com>
2022-09-24 21:16:44 +0300

    added header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faPlane,
+  faCar,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">Motto</h1>
+        <p className="headerDesc">Header description.</p>
+        <button className="headerButton">Sign in / Register</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
