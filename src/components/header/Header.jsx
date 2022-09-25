e6374e665ce600cdf4df39b6218bd246c6259cb6
Raduvi <patruica.radu@gmail.com>
2022-09-25 12:39:51 +0300

    feature/homepagelist added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -70,8 +70,16 @@ const Header = ({ type }) => {
         </div>
         {type !== "list" && (
           <>
-            <h1 className="headerTitle">Motto</h1>
-            <p className="headerDesc">Header description.</p>
+            <h1 className="headerTitle">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            </h1>
+            <p className="headerDesc">
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
             <button className="headerButton">Sign in / Register</button>
             <div className="headerSearch">
               <div className="headerSearchItem">
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">booking</span>
+        <span className="logo">Booking</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
